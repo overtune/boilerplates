b4f3ed645d95f372665445408716a9383b33aff8
Johan Runesson <info@johanrunesson.se>
2023-11-28 11:47:03 +0100

    fix: adjust the linting setup

--- packages/web-component/src/web-component.js
+++ packages/web-component/src/web-component.js
@@ -5,68 +5,68 @@
  * @public
  */
 class WebComponent extends HTMLElement {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    /**
-     * Bind methods to this
-     */
-    this.onClick = this.onClick.bind(this);
+		/**
+		 * Bind methods to this
+		 */
+		this.onClick = this.onClick.bind(this);
 
-    /**
-     * @type {ShadowRoot}
-     */
-    this.shadow = this.attachShadow({ mode: 'open' });
-  }
+		/**
+		 * @type {ShadowRoot}
+		 */
+		this.shadow = this.attachShadow({ mode: 'open' });
+	}
 
-  /**
-   * connectedCallback
-   * Render html/css & attatch event listeners
-   */
-  connectedCallback() {
-    /**
-     * @type {string}
-     */
-    this.text = this.getAttribute('text') || 'My text';
+	/**
+	 * connectedCallback
+	 * Render html/css & attatch event listeners
+	 */
+	connectedCallback() {
+		/**
+		 * @type {string}
+		 */
+		this.text = this.getAttribute('text') || 'My text';
 
-    // Render
-    this.render();
+		// Render
+		this.render();
 
-    /**
-     * @type {HTMLButtonElement | null}
-     */
-    this.button = this.shadow.querySelector('button');
+		/**
+		 * @type {HTMLButtonElement | null}
+		 */
+		this.button = this.shadow.querySelector('button');
 
-    // Attach event listeners
-    if (this.button) {
-      this.button.addEventListener('click', this.onClick);
-    }
-  }
+		// Attach event listeners
+		if (this.button) {
+			this.button.addEventListener('click', this.onClick);
+		}
+	}
 
-  /**
-   * disconnectedCallback
-   * Remove listeners.
-   */
-  disconnectedCallback() {
-    if (this.button) {
-      this.button.removeEventListener('click', this.onClick);
-    }
-  }
+	/**
+	 * disconnectedCallback
+	 * Remove listeners.
+	 */
+	disconnectedCallback() {
+		if (this.button) {
+			this.button.removeEventListener('click', this.onClick);
+		}
+	}
 
-  /**
-   * On click callback.
-   * @param {Event & { target: HTMLButtonElement }} e - on click callback.
-   */
-  onClick(e) {
-    /* eslint-disable-next-line no-console */
-    console.log(e, this.text);
-  }
+	/**
+	 * On click callback.
+	 * @param {Event & { target: HTMLButtonElement }} e - on click callback.
+	 */
+	onClick(e) {
+		/* eslint-disable-next-line no-console */
+		console.log(e, this.text);
+	}
 
-  /**
-   * Renders the component html/css.
-   */
-  render() {
-    this.shadow.innerHTML = `
+	/**
+	 * Renders the component html/css.
+	 */
+	render() {
+		this.shadow.innerHTML = `
     <style>
         :host {
           --color: var(--wc-color, #333);
@@ -81,7 +81,7 @@ class WebComponent extends HTMLElement {
         <button type="button">${this.text}</button>
       </div>
     `;
-  }
+	}
 }
 
 window.customElements.define('web-component', WebComponent);
